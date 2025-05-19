bdc39747b4bd49f189c6aad495b1489b1ba13232
adriiberjaga <adriabdd@gmail.com>
2025-05-19 13:34:01 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 import './style.css'
 
+const apiKey = '0ehrAbe1Xqtj3bjFUiRV6NUJBPnfDZCT';
+const query = 'funny cat';
+const $form = document.querySelector('#form')
+
+    $form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        console.log($form.value) 
+    })
+
+fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=5`)
+  .then(response => response.json())
+  .then(data => {
+    data.data.forEach(gif => {
+      console.log(gif.images.original.url); // URL del GIF
+    });
+  })
+  .catch(err => console.error(err));
