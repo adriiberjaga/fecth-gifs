9939b1ea73b253f316b3dda54032d8618d7d4e7f
adriiberjaga <adriabdd@gmail.com>
2025-05-19 20:48:28 +0200

    buscador de gifs finalizado

--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
-import './style.css'
+import "./style.css";
 
-const apiKey = '0ehrAbe1Xqtj3bjFUiRV6NUJBPnfDZCT';
-const query = 'funny cat';
-const $form = document.querySelector('#form')
-
-    $form.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        console.log($form.value) 
+const apiKey = "0ehrAbe1Xqtj3bjFUiRV6NUJBPnfDZCT";
+const $form = document.querySelector("#form");
+const $input = document.querySelector("#searchInput");
+const $results = document.querySelector('#results')
+const $numberOfGifs = document.querySelector('#numberOfGifs')
+$form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const query = $input.value.trim();
+  let queryNumber = $numberOfGifs.value;
+  if (!query) return;
+  if(!queryNumber) queryNumber = 1
+  fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=${queryNumber}&lang=es`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      $results.innerHTML = ''
+      data.data.forEach((gif) => {
+        console.log(gif.images.original.url); 
+        createGift(gif)
+      });
     })
-
-fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=5`)
-  .then(response => response.json())
-  .then(data => {
-    data.data.forEach(gif => {
-      console.log(gif.images.original.url); // URL del GIF
-    });
-  })
-  .catch(err => console.error(err));
+    .catch((err) => console.error(err));
+});
+function createGift (gif) {
+  const img = document.createElement('img')
+  img.src = gif.images.original.url
+  img.style.width = '200px'
+  $results.appendChild(img)
+}
